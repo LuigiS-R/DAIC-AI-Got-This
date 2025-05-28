8b04d64082c6eb3c96d63a90968f918b0015404a
LuigiS-R <luigisr03@gmail.com>
2025-05-28 11:07:43 +0900

    Creating scores tables
    Table scores is created to store the scores from the students after being graded

--- db/database.js
+++ db/database.js
@@ -4,6 +4,7 @@ const db = new sqlite3.Database('./myDatabase.db');
 
 db.serialize(()=>{
     db.run("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT, email TEXT, password TEXT)");
+    db.run("CREATE TABLE IF NOT EXISTS scores (id INTEGER PRIMARY KEY, name TEXT, score INTEGER)");
 });
 
 module.exports = db;
